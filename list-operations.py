33568eddd81459cd7d2033a790f2237b0242a130
Konstantin Dorichev <KDorichev@gmail.com>
2017-04-01 19:49:05 +0300

    Initial commit

---
+++ list-operations.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+    for i in range(0,N):
+        line = input()
+        args = line.split()
+        if args[0]=='print':
+            print(lst)
+        elif args[0]=='insert':
+            lst.insert(int(args[1]),args[2])
+        elif args[0]=='remove':
+            lst.remove(args[1])
+        elif args[0]=='append':
+            lst.append(args[1])
+        elif args[0]=='sort':
+            lst.sort()
+        elif args[0]=='pop':
+            lst.pop()
+        elif args[0]=='reverse':
+            lst.reverse()
+        else:
+            continue
