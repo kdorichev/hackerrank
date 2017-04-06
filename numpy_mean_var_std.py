3d760b2e0853651bb9bfa1447f55b53d40eed177
Konstantin Dorichev <KDorichev@gmail.com>
2017-04-06 15:33:36 +0300

    Comments added

--- numpy_mean_var_std.py
+++ numpy_mean_var_std.py
@@ -1,12 +1,12 @@
 import numpy as np
+# Read n,m -- two integers from 1st line -- matrix's dimensions
+(n,m) = map(int,input().split()) # Split the string and convert into integer each number with int()
 
-(n,m) = map(int,input().split())
+X = np.zeros((n,m), dtype=int)  # create a 2D zero matrix of integer type values
+for i in range(0,m):            # For each of m rows
+    line = input()              # read next row
+    X[i] = line.split()         # Split and assign; conversion into integer -- implicitly by numpy
 
-X = np.zeros((n,m), dtype=int)
-for i in range(0,m):
-    line = input()
-    X[i] = line.split()
-
-    print(np.mean(X,axis=1))
-    print(np.var(X,axis=0))
-    print(np.std(X))
+    print(np.mean(X,axis=1))    # Calculate mean per each column
+    print(np.var(X,axis=0))     # Calculate variance per each row
+    print(np.std(X))            # Calculate standard deviation for flattened matrix -- all the elements
